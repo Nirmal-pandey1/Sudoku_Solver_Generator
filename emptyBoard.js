64cac78849e4c861d6957be7c1c7477b4defc41a
SHUBHAMJoshi22 <shubhamjoshi8630@gmail.com>
2025-05-27 19:11:54 +0530

    Add files via upload

---
+++ emptyBoard.js
@@ -0,0 +1,5 @@
+// Create a 9x9 empty board (strings)
+export const emptyBoard = Array(9).fill(null).map(() => Array(9).fill(""));
+
+// Create a 9x9 fixed state board (for pre-filled cells)
+export const emptyFixed = Array(9).fill(null).map(() => Array(9).fill(false));

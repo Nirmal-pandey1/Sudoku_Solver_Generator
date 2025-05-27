11bd15a0ec9bbb7b0c61ad46401535e90241b9d6
SHUBHAMJoshi22 <shubhamjoshi8630@gmail.com>
2025-05-27 21:11:39 +0530

    Delete emptyBoard.js

--- emptyBoard.js
+++
@@ -1,5 +0,0 @@
-// Create a 9x9 empty board (strings)
-export const emptyBoard = Array(9).fill(null).map(() => Array(9).fill(""));
-
-// Create a 9x9 fixed state board (for pre-filled cells)
-export const emptyFixed = Array(9).fill(null).map(() => Array(9).fill(false));
